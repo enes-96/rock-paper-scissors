0f0cd5eda1d49d719569a6b70f724fa8a9ce12d5
Seyit Enes <seyit7enes@gmail.com>
2022-11-28 23:09:20 +0100

    done

--- script.js
+++ script.js
@@ -4,19 +4,6 @@ const resultDisplay = document.querySelector("#result");
 const choicesDisplay = document.querySelector("#choices");
 const choices = ["rock", "paper", "scissors"];
 
-const handleClick = (e) => {
-  getResults(
-    e.target.innerHTML,
-    choices[Math.floor(Math.random() * choices.length)]
-  );
-};
-choices.forEach((choice) => {
-  const button = document.createElement("button");
-  button.innerHTML = choice;
-  button.addEventListener("click", handleClick);
-  choicesDisplay.appendChild(button);
-});
-
 const getResults = (userChoice, computerChoice) => {
   switch (userChoice + computerChoice) {
     case "scissorspaper":
@@ -51,3 +38,16 @@ const getResults = (userChoice, computerChoice) => {
       break;
   }
 };
+
+const handleClick = (e) => {
+  getResults(
+    e.target.innerHTML,
+    choices[Math.floor(Math.random() * choices.length)]
+  );
+};
+choices.forEach((choice) => {
+  const button = document.createElement("button");
+  button.innerHTML = choice;
+  button.addEventListener("click", handleClick);
+  choicesDisplay.appendChild(button);
+});
